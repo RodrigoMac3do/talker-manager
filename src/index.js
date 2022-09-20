d7c8d9dd2db15db04b40d7ae5f0c8582ff6f9c26
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-09-20 19:54:06 -0300

    feat: GET busca dados de palestrantes cadastrados

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const readJSON = require('./helpers/read');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +8,18 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar 
+// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// req:1 get para pegar informações de palestrantes cadastrados
+app.get('/talker', (req, res) => {
+  const data = readJSON();
+  if (!data) {
+    res.status(200).json([]);
+  }
+  res.status(200).json(data);
 });
 
 app.listen(PORT, () => {
