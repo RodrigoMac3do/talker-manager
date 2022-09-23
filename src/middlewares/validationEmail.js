9b06dd59ae28dd1772c637948f6ade5cfa322d71
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-09-23 13:07:02 -0300

    fix: criação de variável para status http

--- src/middlewares/validationEmail.js
+++ src/middlewares/validationEmail.js
@@ -1,11 +1,15 @@
+const HTTP_BAD_REQUEST_STATUS = 400;
+
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
   const valid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email);
   if (!email) {
-    res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    res
+      .status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "email" é obrigatório' });
   } else if (!valid) {
     res
-      .status(400)
+      .status(HTTP_BAD_REQUEST_STATUS)
       .json({ message: 'O "email" deve ter o formato "email@email.com"' });
   } else {
     next();
--- src/middlewares/validationPassword.js
+++ src/middlewares/validationPassword.js
@@ -1,11 +1,15 @@
+const HTTP_BAD_REQUEST_STATUS = 400;
+
 const validatePassword = (req, res, next) => {
   const { password } = req.body;
   const valid = /^[0-9a-zA-Z]{6,}$/.test(password);
   if (!password) {
-    res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    res
+      .status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "password" é obrigatório' });
   } else if (!valid) {
     res
-      .status(400)
+      .status(HTTP_BAD_REQUEST_STATUS)
       .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   } else {
     next();
