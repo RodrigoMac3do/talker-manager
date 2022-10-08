5a544299cc46aeba335e18712dc992e99f2f0583
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-07 23:22:14 -0300

    feat: busca talker por id

--- src/models/talker.model.js
+++ src/models/talker.model.js
@@ -2,6 +2,12 @@ const readerJson = require('../utils/read');
 
 const listAll = () => readerJson();
 
+const findById = (id) => {
+  const talkers = readerJson();
+  return talkers.find((talk) => talk.id === id);
+};
+
 module.exports = {
   listAll,
+  findById,
 };
