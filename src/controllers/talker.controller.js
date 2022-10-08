f84ec83d7cd3cb134afcbb0b48ff6e7eb82b836b
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-08 11:18:03 -0300

    feat: controller talker search

--- src/controllers/talker.controller.js
+++ src/controllers/talker.controller.js
@@ -2,13 +2,26 @@ const service = require('../services');
 
 const listAll = async (_req, res, _next) => {
   const talkers = await service.talker.listAll();
+
   res.status(200).json(talkers);
 };
 
 const findById = async (req, res, next) => {
   const id = Number(req.params.id);
+
   try {
     const talker = await service.talker.findById(id);
+    res.status(200).json(talker);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const findByName = async (req, res, next) => {
+  const { q } = req.query;
+
+  try {
+    const talker = await service.talker.findByName(q);
     res.status(200).json(talker);
   } catch (error) {
     next(error);
@@ -18,4 +31,5 @@ const findById = async (req, res, next) => {
 module.exports = {
   listAll,
   findById,
+  findByName,
 };
