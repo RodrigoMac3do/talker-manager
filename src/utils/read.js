ad62fe79fee4e67ce76e7b43488e1298d0b0123c
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-07 23:14:28 -0300

    feat: função faz leitura do arquivo json

---
+++ src/utils/read.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const readerJson = () => {
+  const data = fs.readFileSync('./src/talker.json', 'utf-8');
+  return JSON.parse(data);
+};
+
+module.exports = readerJson;
