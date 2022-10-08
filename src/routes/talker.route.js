7906a73629050366d96d00b1b9082fcf06ce3e49
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-08 11:23:12 -0300

    feat: rota do talker search

--- src/routes/talker.route.js
+++ src/routes/talker.route.js
@@ -2,8 +2,10 @@ const express = require('express');
 const controller = require('../controllers');
 
 const router = express.Router();
+const middleware = require('../middlewares');
 
 router.get('/', controller.talker.listAll);
+router.get('/search', middleware.auth, controller.talker.findByName);
 router.get('/:id', controller.talker.findById);
 
 module.exports = router;
