7edb30f26a7458b997e36a42e4c1ec7396063c1b
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-08 11:22:48 -0300

    feat: model do talker name

--- src/models/talker.model.js
+++ src/models/talker.model.js
@@ -1,13 +1,33 @@
 const readerJson = require('../utils/read');
+const writerJson = require('../utils/write');
 
-const listAll = () => readerJson();
+const listAll = () => {
+  const talkers = readerJson();
+
+  return talkers;
+};
 
 const findById = (id) => {
   const talkers = readerJson();
-  return talkers.find((talk) => talk.id === id);
+
+  const talker = talkers.find((talk) => talk.id === id);
+
+  return talker;
 };
+
+const findByName = (q) => {
+  const talkers = readerJson();
+
+  const talker = talkers.filter((talk) => talk.name.includes(q));
+
+  return talker;
+};
+
+const insert = (file) => writerJson(file);
 
 module.exports = {
   listAll,
   findById,
+  insert,
+  findByName,
 };
