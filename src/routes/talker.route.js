9553c18580f224b7055df840ab6c5349775906a6
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-20 22:30:02 -0300

    feat: criação de rota post para talker

--- src/routes/talker.route.js
+++ src/routes/talker.route.js
@@ -7,5 +7,13 @@ const router = express.Router();
 router.get('/', controller.talker.listAll);
 router.get('/search', middleware.auth, controller.talker.findByName);
 router.get('/:id', controller.talker.findById);
+router.post(
+  '/',
+  middleware.auth,
+  middleware.name,
+  middleware.age,
+  middleware.talk,
+  controller.talker.insert,
+);
 
 module.exports = router;
